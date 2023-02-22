64fd906f89c6736fe09a509e447a12def9b7440e
Viktor_Rus <vaurusov@mts.ru>
2023-02-22 21:09:55 +0300

    added first test

--- cypress/e2e/authorization.cy.js
+++ cypress/e2e/authorization.cy.js
@@ -3,8 +3,9 @@ describe('Authorization', () => {
     cy.visit('/')
     cy.get("input[name='email']").type("email")
     cy.get("input[name='password']").click()
-    cy.xpath("//input[@name='email'] /following::div[1]").then(text=>{
-      expect(text.text()).to.equal("Введите корректный E-Mail")
-    })
+    cy.contains("Введите корректный E-Mail").should("be.visible")
+    // cy.xpath("//input[@name='email'] /following::div[1]").then(text=>{
+    //   expect(text.text()).to.equal("Введите корректный E-Mail")
+    // })
   })
 })
